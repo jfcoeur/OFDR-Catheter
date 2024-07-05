6dc536ac3657a4f6eb3e54987981ef29781bf753
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-05 16:56:43 -0400

    Update straincal.m

--- straincal.m
+++ straincal.m
@@ -1,29 +1,24 @@
-function [x, sig, ite, hh] = straincal(filesref, filessamp, gen, ite, itetot, hh)
+function [x, sig, ite, hh] = straincal(filesref, filessamp, ite, itetot, hh)
 
 numrefs = length(filesref);
+numsamps = length(filessamp);
 
 for r = 1:numrefs
+
     ref = filesref(r);
-    [x, sig(:,:,r), ite, hh] = fun(ref, filessamp, gen, ite, itetot, hh);
-end % r = 1:numrefs
 
-%%%%%%%%
-function [x, sig, ite, hh] = fun(ref, samps, gen, ite, itetot, hh)
+    for s = 1:numsamps
 
-    for s = 1:length(samps)
-
-        samp = samps(s);
-
-        [x, sigout] = wvlshift(ref, samp, gen);
-        sig(s,:) = sigout;
+        samp = filessamp(s);
+        [x, sigout] = wvlshift(ref, samp);
+        sig(s,:,r) = sigout;
 
         % Waitbar
         ite = ite + 1;
-        waitbar(ite/itetot, hh) 
+        waitbar(ite/itetot, hh)         
     
-    end % s = 1:length(samps)
-  
-end % function fun
-%%%%%%%%
+    end % s = 1:numsamps
+    
+end % r = 1:numrefs
 
 end % function straincal
