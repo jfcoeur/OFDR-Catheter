2d3a773e1d48089fead18cd5278340330dd73a58
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-05 16:49:20 -0400

    Update wvlshift.m

--- wvlshift.m
+++ wvlshift.m
@@ -1,6 +1,22 @@
-function [] = wvlshift()
+function [x, sig] = wvlshift(fileref, filesamp)
 
+% Reference and sample data
+ref = importdata(fileref, '\t', 8);
+samp = importdata(filesamp, '\t', 8);
 
+% Light speed and ref. idx.
+c = physconst('LightSpeed');
+n = 1.444;
 
+% Positions
+t = ref.data(:,1);
+x = c/n*t*1e-9/2;
+
+% Wavelengths
+refwav = ref.data(:,3);
+sampwav = samp.data(:,3);
+
+% Strain
+sig = (sampwav - refwav)./refwav;
 
 end % function wvlshift
