ac0c723a2545ee58f4b686896fc9e2cfd9721a68
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-08 11:10:37 -0400

    Create main.m

---
+++ main.m
@@ -0,0 +1,38 @@
+function [] = main(in)
+
+% Initialization %
+clearvars
+clc
+close all
+warning('off','all')
+%%%%%%%%%%%%%%%%%%
+
+% Inputs
+dirdata = in.dirdata;
+gen = in.gen;
+
+results = struct;
+
+for f = 1:3    
+
+    % Directories
+    dirref = dirdata(f) + "/References";
+    dirsamp = dirdata(f) + "/Samples";
+    categories = listfolders(dirsamp);
+    dirsamp = dirsamp + categories;
+
+    [x, sig] = resloop(dirref, dirsamp, categories, gen, in.itetot);
+
+    field = ['fiber' num2str(f)];
+    results.(field) = sig;
+
+end % f = 1:3
+
+% Saving
+results.x = x;
+filename = in.savepath + "_sig.mat";
+save(filename,"results")
+
+disp("Strain task completed");
+
+end % function main
