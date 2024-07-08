bb03d1da43b5ee34506b47a5abeb13ab72745fbd
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-08 11:11:29 -0400

    Create listfolders.m

---
+++ listfolders.m
@@ -0,0 +1,13 @@
+function [folders] = listfolders(path)
+
+list = dir(path);
+list = {list(3:end)};
+folderflags = [list.isdir];
+
+if all(folderflags,'all')
+    folders = "/" + cellfun(@convertCharsToStrings, {list.name});
+else
+    folders = "";
+end
+
+end % function listfolders
