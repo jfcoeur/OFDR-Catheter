3430f8c9efeee4c073f3902a39b481a83b952417
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-08 13:13:06 -0400

    Update step1.m

--- step1.m
+++ step1.m
@@ -1,4 +1,4 @@
-function [g, tet1] = step1(dirdata, gen)
+function [tet1, g] = step1(dirdata, gen)
 
 % Strain signals file
 filename = dirdata + "_sig.mat";
@@ -10,30 +10,14 @@ sig1 = results.fiber1;
 sig2 = results.fiber2;
 sig3 = results.fiber3;
 
-% Exclude for validation
-exclude = [1, 2, 3];
+% Curvatures
+curv = gen.curv;
 
-% Samples loop
-i = 1;
+% Curvature loop
 for c = 1:length(curv)
 
-    k = curv(c);
-    bool = ismember(k, exclude);
-    
-    if bool
-        continue;
-    end
-    
-    % Experimental gauge
+    k = curv(c);    
     g = gauge(gen, sig1, sig2, sig3, c);
-    
-    % Gauge fitting
-    [exp2(i,:,:), poly3(i,:,:)] = gtrend(gen, x, g, curv(c));
-    
-    % Geometrical parameters
-    Mout{:,i} = geoparam(gen, sig1, sig2, sig3, x, g, c);
-    
-    i  = i + 1;
     
 end % c = 1:length(curv)
 
