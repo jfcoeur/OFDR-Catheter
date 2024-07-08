22f3df93471f6e24cbc0ebe78482fe8389d815e2
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-08 11:23:02 -0400

    Update general.m

--- general.m
+++ general.m
@@ -5,7 +5,11 @@ gen.wino = [9 3 2 1 1]; % Outlier window sizes
 gen.wins = [9 3 2 1 1]; % Smoothing window sizes
 
 % Geometrical parameters
-
+gen.r1 = 70e-6; % [m]  % 89.5e-6
+gen.r2 = r1; % [m] 
+gen.r3 = r1; % [m] 
+gen.tet12 = 240; % [deg] 
+gen.tet23 = -120; % [deg]
 
 % Indices
 idx_full = [41:410; 10:379; 15:384];
