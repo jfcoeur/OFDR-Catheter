a70f9b315a2c6cb745090800a948053aa8cb61dd
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-08 13:54:26 -0400

    Update main.m

--- main.m
+++ main.m
@@ -6,26 +6,18 @@ clc
 close all
 warning('off','all')
 
-in.gen.ang = in.gen.angles(in.dataset,:);
-in.gen.curv = in.gen.curvatures(in.dataset,:);
+% Calculation
+[g, tet1] = catloop(in.dirdata, in.gen);
 
-% Step
-switch in.step
-    case 1
-        [out.M, out.exp2, out.poly3] = step1(in.dirdata, in.gen);
-        
-    case 2
-        [small, large, moy] = step2(in.M, in.exp2, in.poly3, in.gen);
-        
-        calib = struct('geo', moy, 'scoef', small, 'lcoef', large);
-        filename = in.savepath + "_calib.mat";
-        save(filename, "calib");
+% Averaging
 
-        out = [];
-        disp("Calibration task completed");
 
-    otherwise
-      disp('Error')
-end
+% Saving
+calib = struct('g', g, 'tet1', tet1);
+filename = in.savepath + "_calib.mat";
+save(filename, "calib");
+
+out = [];
+disp("Calibration task completed");
 
 end % function main
