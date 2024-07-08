fab9bdb51fa4a1ee679d14c4f4fed105ce47f3ee
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-08 13:25:24 -0400

    Create gauge.m

---
+++ gauge.m
@@ -0,0 +1,45 @@
+function [g, tet1ave] = gauge(gen, s1, s2, s3, c)
+
+% Initial parameters
+alpha = 0;
+r1 = gen.r1; % [m]  
+r2 = gen.r2; % [m] 
+r3 = gen.r3; % [m] 
+tet12 = gen.tet12; % [deg] 
+tet23 = gen.tet23; % [deg]
+
+% Exclude for validation
+exclude = [1, 2, 3];
+samples = setdiff(1:8,exclude);
+
+for r = 1:length(gen.res)
+
+    f1 = s1{r};
+    f2 = s2{r};
+    f3 = s3{r};
+
+    k = [];
+    tet1 = [];
+
+    for ref = 1:size(f1,3)
+
+        i = i + 1;
+        for a = samples
+        
+        	sig1 = f1(a,:,ref,c);
+        	sig2 = f2(a,:,ref,c);
+        	sig3 = f3(a,:,ref,c);
+        	
+        	[k(i,:,ref), tet1(i,:,ref)] = k&tet1(alpha, sig1, sig2, sig3, r1, r2, r3, tet12, tet23);
+        	
+        	kave = mean(k,1);
+        	tet1ave{r} = mean(tet1,1);
+        	g{r} = kave./gen.curv(c);
+        
+        end % a = samples
+    
+    end ref = 1:size(f1,3)
+    
+end % r = 1:length(gen.res)
+
+end % function gauge
