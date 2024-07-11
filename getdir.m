f6bfcf10c272fd3e29a174d95dc80d1ce9572b90
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-11 17:14:21 -0400

    Create getdir.m

---
+++ getdir.m
@@ -0,0 +1,58 @@
+function [directories] = getdir(task, loadname, savename)
+
+switch filesep
+    case "\"
+        dirmain = "C:\Users\jfcoe\OneDrive - Johns Hopkins\Documents\Hopkins\GitHub\";        
+    case "/"
+        dirmain = "/home/jfcoeur/Documents/";
+    otherwise
+        disp('Error in operating system')
+end
+
+% Matlab directory
+dircodes = dirmain + "Matlab codes" + filesep;
+
+% Task directory
+dirtask = dircodes + task + filesep;
+
+% Needle directory
+dircatheter = dirmain + "Distributed catheter" + filesep;
+
+% Raw data directory
+dirraw = dircatheter + "Raw data" + filesep;
+
+if task == "Strain"
+    fibers = fun(dirraw);
+    dirraw = dirraw + fibers;
+end
+
+% Processed data directory
+dirproc = dircatheter + "Processed data" + filesep;
+
+% Loading directory
+dirload = dirproc + loadname;
+
+% Saving directory
+dirsave = dirproc + savename;
+
+% Output structure
+directories = struct('functions', dirtask, 'raw', dirraw, 'processed', dirproc, 'load', dirload, 'save', dirsave);
+
+%%%%%%%%
+function [fibers] = fun(direct)
+
+list = dir(direct);
+list = {list(3:end)};
+folderflags = [list.isdir];
+numf = length(folderflags);
+
+if all(folderflags,'all') && numF == 3
+    fibers = cellfun(@convertCharsToStrings, {list.name}) + filesep;
+else
+    fibers = "";
+end
+
+end % function fiberpaths
+%%%%%%%%
+
+end % function getdir
