a9f392b2f7f7b3cc8b2e9a506c149793b3a0c75d
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-12 12:18:52 -0400

    Update kcorr.m

--- kcorr.m
+++ kcorr.m
@@ -1,14 +1,10 @@
-function [kout] = kcorr(dirload, k, r)
+function [k] = kcorr(dirload, k, r, ref)
 
 % Gauge
 filecalib = dirload + "_calib.mat";
 g = load(filecalib).g{r};
 
 % Curvature correction
-for ref = 1:size(g,2)
-
-    kout(ref,:) = k./g(:,ref);
-
-end % ref = 1:size(g,2)
+k = k./g(:,ref);
 
 end % function kcorr
