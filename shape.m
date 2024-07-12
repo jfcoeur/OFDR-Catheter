2e8ab6896700ff3718f62c9fa8a65a070c452e6c
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-12 12:34:21 -0400

    Update shape.m

--- shape.m
+++ shape.m
@@ -1,50 +1,52 @@
-function [xyz, k, alpha, err] = shape(gen, xin, s1, s2, s3, moy, c)
+function [xyz, k, alpha, err] = shape(dirload, gen, xin, s1, s2, s3, c)
 
-ang = -gen.ang;
+% Calibration parameters
+tet1 = gen.tet1;
+tet12 = gen.tet12;
+tet23 = gen.tet23;
+r1 = gen.r1;
+r2 = gen.r2;
+r3 = gen.r3;
 
-for r = 1:length(gen.res)    
+for r = 1:length(gen.res)
 
     f1 = s1{r};
     f2 = s2{r};
     f3 = s3{r};
     x = xin{r};
-
-    % Calibration parameters
-    calib = moy{r};
-    tet1 = calib(1,:);
-    tet12 = calib(2,:);
-    tet23 = calib(3,:);
-    r1 = calib(4,:);
-    r2 = calib(5,:);
-    r3 = calib(6,:);    
-
+    
     kout = [];
     alphaout = [];
-    xyzout = [];    
-    
-    for a = 1:length(ang)
-    
-    	sig1 = f1(a,:,c);
-    	sig2 = f2(a,:,c);
-    	sig3 = f3(a,:,c);
+    xyzout = [];
 
-        % Curvature & orientation
-        [ktemp, alphaout(a,:)] = k&alpha(sig1, sig2, sig3, r1, r2, r3, tet1, tet12, tet23);
+    for ref = 1:size(f1,3)
 
-        % Curvature correction
-        kout(a,:) = kcorr(gen, ktemp, r);
+        for s = 1:size(f1,1)
+
+         	sig1 = f1(s,:,ref,c);
+        	sig2 = f2(s,:,ref,c);
+        	sig3 = f3(s,:,ref,c);   
+         
+            % Curvature & orientation
+            [ktemp, alphaout(s,:,ref)] = k&alpha(sig1, sig2, sig3, r1, r2, r3, tet1, tet12, tet23);
+    
+            % Curvature correction
+            kout(s,:,ref) = kcorr(dirload, k, r, ref);
+            
+            % 3D coordinates
+            xyzout(s,:,:,ref) = 3Dcoord(x, kout(s,:,ref), alphaout(s,:,ref))     
         
-        % 3D coordinates
-        xyzout(a,:,:) = 3Dcoord(x, kout(a,:), alphaout(a,:))
-    	    
-    end % a = 1:length(gen.ang)
+        end % s = 1:size(f1,1)
+
+        % Errors
+        errout(:,:,ref) = errors(gen, x, xyzout(:,:,:,ref), kout(:,:,ref), alphaout(:,:,ref), c);
     
+    end % ref = 1:size(f1,3)
+
     k{r} = kout;
     alpha{r} = alphaout;
     xyz{r} = xyzout;
-    
-    % Errors
-    err{r} = errors(gen, x, xyzout, kout, alphaout, c);    
+    err{r} = errout;
     
 end % r = 1:length(gen.res)
 
