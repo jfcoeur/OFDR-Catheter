8912afb32d5988700cd6d4f0ece5ff3cc86a3694
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-16 19:28:10 -0400

    Update getidx.m

--- getidx.m
+++ getidx.m
@@ -1,6 +1,16 @@
-function [] = getidx()
+function [gen] = getidx(gen, field, f)
 
+switch field
+    case or('Full', 'Large', 'Medium', 'Small')
+        idx_temp = gen.Full;
+    case or('Sinus', 'Sinus2D', 'Sinus3D')
+        idx_temp = gen.Sinus;
+    case or('Exponential', 'Exponential2D', 'Exponential3D')
+        idx_temp = gen.Exponential;
+    otherwise
+        disp('Field error')    
+end
 
-
+gen.idx = idx_temp(f,:);
 
 end % function getidx
