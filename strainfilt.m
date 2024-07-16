d0d6687b83a20ba703ee6d7fe22f68ce5cce15fa
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-16 12:46:44 -0400

    Update strainfilt.m

--- strainfilt.m
+++ strainfilt.m
@@ -3,12 +3,12 @@ function [x, sigout] = strainfilt(r, x, sig, gen)
 res = gen.res(r);
 lx = x(2) - x(1);
 sampling = round(res/lx);
+x = x(1:sampling:end);
 
 for s = 1:size(sig,1)
 
     for ref = 1:size(sig,3)
     
-        x = x(1:sampling:end);
         sigout(s,:,ref) = sig(s,1:sampling:end,ref);
         sigout(s,:,ref) = filloutliers(sigout(s,:,ref), "nearest", "movmedian", gen.wino(r));
         sigout(s,:,ref) = smoothdata(sigout(s,:,ref), "rlowess", gen.wins(r));        
