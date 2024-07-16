68c66023ff76961571d17494ca60046a8c647950
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-16 11:29:39 -0400

    Update general.m

--- general.m
+++ general.m
@@ -3,7 +3,6 @@ function [gen] = general()
 gen.res = [0.001 0.003 0.005 0.008 0.01]; % Resolution [m]
 gen.wino = [9 3 2 1 1]; % Outlier window sizes
 gen.wins = [9 3 2 1 1]; % Smoothing window sizes
-gen.curvatures = [0.5 1.6 2.0 2.5 3.2 4.0; 0.25 0.8 1.0 1.25 3.125 NaN];
 
 % Geometrical parameters
 gen.r1 = 70e-6; % [m]  % 89.5e-6
