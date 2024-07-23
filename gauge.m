8f7f6b393ca56a201525e1247d97a1193b59ee81
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-23 11:05:14 -0400

    Update gauge.m

--- gauge.m
+++ gauge.m
@@ -42,7 +42,9 @@ for r = 1:length(gen.res)
         g_temp(:,ref) = kave./gen.curv(c);
     
     end % ref = 1:size(f1c,3)
-    
+
+    g{r} = g_temp;
+    tet1ave{r} = tet1ave_temp;    
     % [g{r}, tet1ave{r}] = paramfilt(r, g_temp, tet1ave_temp, gen);    
         
 end % r = 1:length(gen.res)
