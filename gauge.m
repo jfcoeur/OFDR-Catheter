54f4d28f8763b1d3060a6045318d720c656cfa12
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-23 10:03:38 -0400

    Update gauge.m

--- gauge.m
+++ gauge.m
@@ -36,12 +36,12 @@ for r = 1:length(gen.res)
         end % a = 1:size(sig1,1)
 
         kave = mean(k,1);
-        tet1ave(:,ref) = mean(tet1,1);
-        g(:,ref) = kave./gen.curv(c);
+        tet1ave_temp(:,ref) = mean(tet1,1);
+        g_temp(:,ref) = kave./gen.curv(c);
     
     end % ref = 1:size(f1,3)
     
-    [g{r}, tet1ave{r}] = paramfilt(r, g, tet1ave, gen);    
+    [g{r}, tet1ave{r}] = paramfilt(r, g_temp, tet1ave_temp, gen);    
         
 end % r = 1:length(gen.res)
 
