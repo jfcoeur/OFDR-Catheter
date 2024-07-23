6badf1a3cbe7091bf9d4be566dc5b2d8b9fb30fb
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-07-23 11:33:40 -0400

    Update main.m

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 function [out] = main(in)
 
-[out.xyz, out.k, out.alpha, out.err] = kloop(in.dirload, in.gen)
+[out.xyz, out.k, out.alpha, out.err] = kloop(in.dirload, in.gen);
 
 % Step
 disp("Validation task completed");
