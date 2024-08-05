23fcfd0606d0795cd0acac572cd8e383ac6103e3
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-08-05 17:07:44 -0400

    Add files via upload

--- errors.m
+++ errors.m
@@ -1,32 +1,35 @@
-function [Terr] = errors(gen, x, xyz, k, alpha, c)
+function [Merr, xyzr, xyze] = errors(gen, x, xyz, k, alpha, c)
 
-alphar = zeros(size(x));
+x = x';
+alphar = 360*ones(size(x));
 kr = gen.curv(c)*ones(size(x));
-rownames = {'err_ave' 'err_std' 'tip_ave' 'tip_std' 'rmse_ave' 'rmse_std'};
-colnames = {'xyz' 'k' 'alpha'};
 
-% Theoretical shape
-for s = 1:size(xyz,1)
-    
-    xyzr = tricoord(x, kr, alphar);
-    xyze = reshape(xyz(s,:,:), size(xyzr));
-    [err_xyz(s,:), rmse_xyz(s)] = fun(xyzr, xyze);
-    [err_k(s,:), rmse_k(s)] = fun(kr, k(s,:));
-    [err_alpha(s,:), rmse_alpha(s)] = fun(alphar, alpha(s,:));
+xyzr = tricoord(x, kr, alphar);
+xyze = reshape(xyz, size(xyzr));
 
-end % s = 1:size(xyz,1)
+% Test
+xyze = pointCloud(xyze');
+xyzr_temp = pointCloud(xyzr');
+[~,output] = pcregistericp(xyze,xyzr_temp);
+xyze = output.Location;
+xyze = xyze';
+%
+
+[err_xyz, rmse_xyz] = fun(xyzr, xyze);
+[err_k, rmse_k] = fun(kr, k);
+[err_alpha, rmse_alpha] = fun(alphar, alpha);
 
 colxyz = funcall(err_xyz, rmse_xyz);
 colk = funcall(err_k, rmse_k);
 colalpha = funcall(err_alpha, rmse_alpha);
 
-Terr = table(colxyz, colk, colalpha,'VariableNames', colnames, 'RowNames', rownames);
+Merr = [colxyz, colk, colalpha];
 
 %%%%%%%%
 function [err, rmse] = fun(truth, exp)
 
     subs = truth - exp;    
-    err2 = sum(subs.^2, 2);
+    err2 = sum(subs.^2, 1);
     err = sqrt(err2);
     rmse = sqrt(sum(err2)/length(err2)); 
 
@@ -36,12 +39,10 @@ end % function fun
 %%%%%%%%
 function [out] = funcall(err, rmse)
 
-    out{1} = mean(err,"all");
-    out{2} = std(err,0,"all");
-    out{3} = mean(err(:,end));
-    out{4} = std(err(:,end));
-    out{5} = mean(rmse);
-    out{6} = std(rmse);
+    out(1) = mean(err);
+    out(2) = std(err);
+    out(3) = err(end);
+    out(4) = rmse;
     out = out';
 
 end % function funcall
