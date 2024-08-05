0400baabdbfeb6dd451bf81c537e1e91bf12a995
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-08-05 17:25:22 -0400

    Update refmin.m

--- refmin.m
+++ refmin.m
@@ -1,9 +1,11 @@
-function [] = refmin(moy, Merr, k, alpha, xyze)
+function [xyz_min, k_min, alpha_min] = refmin(moy, Merr, k, alpha, xyze)
 
 k_min = [];
 alpha_min = [];
 xyz_min = [];
 
 [~,I] = min(moy,[],2);
+
+
 
 end % function refmin
