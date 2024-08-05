5bd2adde2c540f290661c7fe80c3107b9a0c5671
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-08-05 17:29:37 -0400

    Update refmin.m

--- refmin.m
+++ refmin.m
@@ -1,4 +1,4 @@
-function [xyz_min, k_min, alpha_min] = refmin(moy, Merr, k, alpha, xyze)
+function [err, xyz_min, k_min, alpha_min] = refmin(moy, Merr, k, alpha, xyze, r)
 
 k_min = [];
 alpha_min = [];
@@ -6,6 +6,19 @@ xyz_min = [];
 
 [~,I] = min(moy,[],2);
 
+for i = 1:size(Merr,3)
 
+    % Errors
+    err(:,:,i,r) = Merr(:,:,i,I(i));
+    
+    % k & alpha
+    k_min(i,:) = k(i,:,I(i));
+    alpha_min(i,:) = alpha(i,:,I(i));
+    
+    % 3D coordinates
+    xyz = xyze(i,:,:,I(i));
+    xyz_min(i,:,:) = reshape(xyz, [3,size(xyz,3)]);   
+
+end % i = 1:size(Merr,3)
 
 end % function refmin
