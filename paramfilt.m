67b4217ee771ecbeab3b189816b291a569273246
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-08-07 13:05:39 -0400

    Add files via upload

--- paramfilt.m
+++ paramfilt.m
@@ -4,11 +4,11 @@ for ref = 1:size(g,2)
 
     % Remove outliers
     g(:,ref) = filloutliers(g(:,ref),"center");
-    % tet1(:,ref) = filloutliers(tet1(:,ref),"center");
+    tet1(:,ref) = filloutliers(tet1(:,ref),"center");
     
     % Smoothing
     g(:,ref) = smoothdata(g(:,ref),"sgolay", gen.wins(r));    
-    % tet1(:,ref) = smoothdata(tet1(:,ref),"sgolay", gen.wins(r));
+    tet1(:,ref) = smoothdata(tet1(:,ref),"sgolay", gen.wins(r));
 
 end % ref = 1:size(g,2)
     
