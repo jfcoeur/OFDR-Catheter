25ff24d9428c68d141b82955605f330d4547e823
Jacynthe Francoeur <jfcoeur@hotmail.com>
2024-09-11 13:47:12 -0400

    Add files via upload

--- errorplot.m
+++ errorplot.m
@@ -1,33 +1,33 @@
 function [out] = errorplot(curv, err)
 
-% temp2 = []; % ajout
+temp2 = []; % ajout
 
 for c = 1:3
 
-    % temp = []; % ajout
+    temp = []; % ajout
 
     for r = 1:5
         out = reshape(err(c,4,1,:,r), [1,size(err,4)]);
 
         if c == 3
-            temp = out(1:5); % ajout (r,:)
+            temp(r,:) = out(1:5); % ajout (r,:)
         else
-            temp = out; % ajout (r,:)
+            temp(r,:) = out; % ajout (r,:)
         end
 
-        rmse_ave(c,r) = mean(temp);
-        rmse_std(c,r) = std(temp);        
+        % rmse_ave(c,r) = mean(temp);
+        % rmse_std(c,r) = std(temp);        
     end
 
     % rmse_ave(c) = mean(temp,"all"); % ajout
-    % rmse_std(c) = std(temp,0,"all");  % ajout
+    % rmse_std(c) = std(temp,0,"all"); % ajout
 
-    % temp2 = [temp2, temp]; % ajout
+    temp2 = [temp2, temp]; % ajout
 
 end
 
-% rmse_ave = mean(temp2,2); % ajout
-% rmse_std = std(temp2,0,2);  % ajout
+rmse_ave = mean(temp2,"all"); % ajout
+rmse_std = std(temp2,0,"all");  % ajout
 
 out = [];
 
@@ -38,10 +38,10 @@ end
 
 legend('1 mm','3 mm','5 mm','8 mm','10 mm')
 xlabel('Curvature [m^{-1}]', 'FontSize', 40)
-ylabel('Error [mm]', 'FontSize', 40)
-xlim([0,30])
+ylabel('RMSE [mm]', 'FontSize', 40)
+xlim([5,30])
 set(gca, 'FontSize', 40)
-title("Valdiation Results", 'FontSize', 40)
-subtitle("RMSE", 'FontSize', 35)
+title("Catheter valdiation results", 'FontSize', 40)
+% subtitle("RMSE", 'FontSize', 35)
 
 end % function   
